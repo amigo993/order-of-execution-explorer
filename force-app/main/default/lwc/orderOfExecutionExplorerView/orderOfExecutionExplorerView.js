1750df6d645f80d6b687bf91d0570852e1c18441
Andrey Migachev <amigo9933@gmail.com>
2024-03-31 20:09:30 +0200

    Named credentials auth for Tooling API

--- force-app/main/default/lwc/orderOfExecutionExplorerView/orderOfExecutionExplorerView.js
+++ force-app/main/default/lwc/orderOfExecutionExplorerView/orderOfExecutionExplorerView.js
@@ -3,6 +3,10 @@ import getOrderOfExecution from '@salesforce/apex/OrderOfExecutionController.get
 
 export default class OrderOfExecutionExplorerView extends LightningElement {
 	@track inputValue = '';
+	//TODO: return Map of headers to List<String> of classes, etc. to be called from Apex
+	headers = ['Validation Rules'];
+	sections;
+	showList = false;
 
     handleInputChange(event) {
         this.inputValue = event.target.value;
@@ -12,7 +16,20 @@ export default class OrderOfExecutionExplorerView extends LightningElement {
         // alert('Input value submitted: ' + this.inputValue);
 		try {
 			let result = await getOrderOfExecution({recordId: this.inputValue});
+			let i = 1;
+			result = result.map(element => {
+				return {id: i++, name: element};
+			});
+
+			this.sections = [
+				{
+					id: 1,
+					header: 'Validation Rules',
+					items: result 
+				}
+			];
 			console.log(result);
+			this.showList = true;
 		} catch (error) {
 			console.error(error);
 		}
