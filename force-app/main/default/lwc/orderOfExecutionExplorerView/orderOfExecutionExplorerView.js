b50eab9b9ef4db06369707a7484f6fe13b655661
Andrey Migachev <amigo9933@gmail.com>
2024-04-01 15:37:17 +0200

    added flows

--- force-app/main/default/lwc/orderOfExecutionExplorerView/orderOfExecutionExplorerView.js
+++ force-app/main/default/lwc/orderOfExecutionExplorerView/orderOfExecutionExplorerView.js
@@ -5,7 +5,7 @@ export default class OrderOfExecutionExplorerView extends LightningElement {
 	@track inputValue = '';
 	//TODO: return Map of headers to List<String> of classes, etc. to be called from Apex
 	headers = ['Validation Rules'];
-	sections;
+	sections = [];
 	showList = false;
 
     handleInputChange(event) {
@@ -16,19 +16,34 @@ export default class OrderOfExecutionExplorerView extends LightningElement {
         // alert('Input value submitted: ' + this.inputValue);
 		try {
 			let result = await getOrderOfExecution({recordId: this.inputValue});
-			let i = 1;
-			result = result.map(element => {
-				return {id: i++, name: element};
-			});
-
-			this.sections = [
-				{
-					id: 1,
-					header: 'Validation Rules',
-					items: result 
-				}
-			];
 			console.log(result);
+
+			let headers = Object.keys(result);
+			let id1 = 1;
+			let id2 = 1;
+
+			for (let index = 0; index < headers.length; index++) {
+				const header = headers[index];
+				const sectionItems = result[header];
+				let id = 1;
+				this.sections.push({
+					id: index,
+					header: header,
+					items: sectionItems.map(sectionItem => {return {id: id++, name: sectionItem};})
+				});
+			}
+
+			// result = result.map(element => {
+			// 	return {id: i++, name: element};
+			// });
+
+			// this.sections = [
+			// 	{
+			// 		id: 1,
+			// 		header: 'Validation Rules',
+			// 		items: result 
+			// 	}
+			// ];
 			this.showList = true;
 		} catch (error) {
 			console.error(error);
